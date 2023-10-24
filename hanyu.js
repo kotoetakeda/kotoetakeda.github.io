02f3263039d63a6e489dd5eb07cac65cbfd29763
kotoetakeda <kotoetakeda0116@gmail.com>
2023-10-24 19:43:06 -0400

    changed font size to fit screen size

--- hanyu.js
+++ hanyu.js
@@ -1,20 +1,20 @@
 function reset() {
-    desc = document.getElementsByClassName('desc');
-    let i = 0;
-    while (i < desc.length) {
-      id = desc[i].id;
-      document.getElementById(id+"_tag").style.listStyle = "disc";
-      document.getElementById(id+"_tag").style.listStylePosition = "inside";
-      document.getElementById(id+"_tag").style.fontSize = "22px";
-      document.getElementById(id+"_tag").style.fontWeight = "normal";
-      document.getElementById(id).style.display = "none";
-      i++;
-    }
-  }
-  function selection(id) {
-    document.getElementById(id+"_tag").style.listStyle = "circle";
+  desc = document.getElementsByClassName('desc');
+  let i = 0;
+  while (i < desc.length) {
+    id = desc[i].id;
+    document.getElementById(id+"_tag").style.listStyle = "disc";
     document.getElementById(id+"_tag").style.listStylePosition = "inside";
-    document.getElementById(id+"_tag").style.fontSize = "35px";
-    document.getElementById(id+"_tag").style.fontWeight = "bold";
-    document.getElementById(id).style.display = "block";
+    document.getElementById(id+"_tag").style.fontSize = "22px";
+    document.getElementById(id+"_tag").style.fontWeight = "normal";
+    document.getElementById(id).style.display = "none";
+    i++;
   }
+}
+function selection(id) {
+  document.getElementById(id+"_tag").style.listStyle = "circle";
+  document.getElementById(id+"_tag").style.listStylePosition = "inside";
+  document.getElementById(id+"_tag").style.fontSize = "32px";
+  document.getElementById(id+"_tag").style.fontWeight = "bold";
+  document.getElementById(id).style.display = "block";
+}
