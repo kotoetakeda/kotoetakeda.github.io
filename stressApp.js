1175d707ce16fe661681e04c0e2f361cf33ea25c
kotoetakeda <kotoetakeda0116@gmail.com>
2023-11-14 11:46:51 -0500

    all minor changes made until today

--- stressApp.js
+++ stressApp.js
@@ -3,18 +3,18 @@ function reset() {
   let i = 0;
   while (i < desc.length) {
     id = desc[i].id;
-    document.getElementById(id+"_tag").style.listStyle = "disc";
-    document.getElementById(id+"_tag").style.listStylePosition = "inside";
-    document.getElementById(id+"_tag").style.fontSize = "22px";
+    // document.getElementById(id+"_tag").style.listStyle = "disc";
+    // document.getElementById(id+"_tag").style.listStylePosition = "inside";
+    document.getElementById(id+"_tag").style.fontSize = "20px";
     document.getElementById(id+"_tag").style.fontWeight = "normal";
     document.getElementById(id).style.display = "none";
     i++;
   }
 }
 function selection(id) {
-  document.getElementById(id+"_tag").style.listStyle = "circle";
-  document.getElementById(id+"_tag").style.listStylePosition = "inside";
-  document.getElementById(id+"_tag").style.fontSize = "30px";
-  document.getElementById(id+"_tag").style.fontWeight = "bold";
-  document.getElementById(id).style.display = "block";
+  const tag = document.getElementById(id+"_tag");
+  const desc = document.getElementById(id);
+  tag.style.fontSize = "32px";
+  tag.style.fontWeight = "bold";
+  desc.style.display = "block";
 }
