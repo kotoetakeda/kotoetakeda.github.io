90eba34c055e768a8bdfad29d420998f137aadc0
kotoetakeda <kotoetakeda0116@gmail.com>
2024-02-25 17:55:16 -0500

    added resume

--- global.js
+++ global.js
@@ -6,3 +6,13 @@ function modal(btn) {
         modal_block.style.display = "none";
     }
 };
+
+/**
+ * Opens the PDF version of the file the button click asks for
+ * @param {string} name the name of the pdf that the button click is asking for
+ */
+function openPDF(name) {
+    if (name == "resume") {
+        window.open("Kotoe Takeda's Resume.pdf", "_blank");
+    }
+}
